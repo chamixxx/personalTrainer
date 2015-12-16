a3e55eb5a99002f36b91640f45df4a381221ed08
Othmane Chami khazraji <othmane.chami@gmail.com>
2015-12-16 16:42:27 +0100

    Added Rss feeds on result page but need to do it with URL fetch service

--- war/js/ctrls/resultsCtrl.js
+++ war/js/ctrls/resultsCtrl.js
@@ -1,10 +1,23 @@
 angular.module('trainerApp').controller('resultsCtrl',resultsCtrlFnt);
 
-resultsCtrlFnt.$inject=['$scope', '$log', '$window', '$cookies', 'comm'];
+resultsCtrlFnt.$inject=['$scope', '$log', '$window', '$cookies', 'comm', 'FeedService'];
 
-function resultsCtrlFnt($scope, $log, $window, $cookies, comm) {
+function resultsCtrlFnt($scope, $log, $window, $cookies, comm, Feed) {
 	$scope.keyWord = $cookies.get('keyWord');
 	$scope.keyWord2 = "";
+	$scope.feedSrc = 'http://www.bodybuilding.com/rss/articles'
+
+	$scope.loadFeed=function(){        
+        Feed.parseFeed($scope.feedSrc).then(function(res){
+        	console.log("hey");
+            $scope.feeds=res.data.responseData.feed.entries;
+                    console.log($scope.feeds);
+
+        });
+
+    }
+
+    $scope.loadFeed();
 
 	var future = comm.getSearchResults($scope.keyWord);
 	future.then(
